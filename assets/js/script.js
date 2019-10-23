c95ff6a86bcc8a95005a53153cb1ebd72d7a2c2b
thyago_lbt <thyago14@outlook.fr>
2019-10-23 11:42:01 +0200

    fin

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function test() {
+  var lastName = document.getElementById('lastname').value;
+  var firstName = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  alert(" Nom : " + lastName + "\n Prénom : " + firstName + "\n Ville : " + city);
+}
